0e7074b1595fd44551c6eaddcbd6c78e33e5909f
timaiv <timaivv27@gmail.com>
2018-07-01 11:15:41 +0300

    -default comments

--- TimaivAddIn/Ribbon/Ribbon1.cs
+++ TimaivAddIn/Ribbon/Ribbon1.cs
@@ -7,35 +7,20 @@ using System.Runtime.InteropServices;
 using System.Text;
 using Office = Microsoft.Office.Core;
 
-// TODO:  Follow these steps to enable the Ribbon (XML) item:
-
-// 1: Copy the following code block into the ThisAddin, ThisWorkbook, or ThisDocument class.
-
-//  protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
-//  {
-//      return new Ribbon1();
-//  }
-
-// 2. Create callback methods in the "Ribbon Callbacks" region of this class to handle user
-//    actions, such as clicking a button. Note: if you have exported this Ribbon from the Ribbon designer,
-//    move your code from the event handlers to the callback methods and modify the code to work with the
-//    Ribbon extensibility (RibbonX) programming model.
-
-// 3. Assign attributes to the control tags in the Ribbon XML file to identify the appropriate callback methods in your code.  
-
-// For more information, see the Ribbon XML documentation in the Visual Studio Tools for Office Help.
-
-
 namespace TimaivAddIn
 {
     [ComVisible(true)]
     public class Ribbon1 : Office.IRibbonExtensibility
     {
+        #region Private Members
         private Office.IRibbonUI ribbon;
+        #endregion
 
+        #region Constructor
         public Ribbon1()
         {
         }
+        #endregion
 
         #region IRibbonExtensibility Members
         public string GetCustomUI(string ribbonID)
