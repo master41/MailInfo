ef5185c09a3beb1583bb80caccdb4aa984efaacb
timaiv <timaivv27@gmail.com>
2018-07-02 22:59:18 +0300

    *OutlookUtils

--- TimaivAddIn/Utils/OutlookUtils.cs
+++ TimaivAddIn/Utils/OutlookUtils.cs
@@ -9,6 +9,7 @@ namespace TimaivAddIn
     {
         #region Constants
         private const string PR_TRANSPORT_MESSAGE_HEADERS = "http://schemas.microsoft.com/mapi/proptag/0x007D001E";
+        private const string PR_SMTP_ADDRESS = @"http://schemas.microsoft.com/mapi/proptag/0x39FE001E";
         #endregion
 
         #region Methods
@@ -70,6 +71,174 @@ namespace TimaivAddIn
                     propertyAccessor = null;
                 }
             }
+        }
+
+        internal static string GetSenderSMTPAddress(this Outlook.MailItem _mail)
+        {
+            if (_mail == null) throw new ArgumentNullException();
+
+            if (_mail.SenderEmailType == "EX")
+            {
+                Outlook.AddressEntry sender = null;
+                try
+                {
+                    sender = _mail.Sender;
+                    if (sender != null)
+                    {
+                        //Now we have an AddressEntry representing the Sender
+                        if (sender.AddressEntryUserType == Outlook.OlAddressEntryUserType.olExchangeUserAddressEntry
+                         || sender.AddressEntryUserType == Outlook.OlAddressEntryUserType.olExchangeRemoteUserAddressEntry)
+                        {
+                            //Use the ExchangeUser object PrimarySMTPAddress
+                            Outlook.ExchangeUser exchUser = null;
+                            try
+                            {
+                                exchUser = sender.GetExchangeUser();
+                                if (exchUser != null)
+                                {
+                                    return exchUser.PrimarySmtpAddress;
+                                }
+                                else
+                                {
+                                    return null;
+                                }
+                            }
+                            finally
+                            {
+                                if (exchUser != null)
+                                {
+                                    Marshal.ReleaseComObject(exchUser);
+                                    exchUser = null;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Outlook.PropertyAccessor propertyAccessor = null;
+                            try
+                            {
+                                propertyAccessor = sender.PropertyAccessor;
+                                if (propertyAccessor != null)
+                                {
+                                    try
+                                    {
+                                        return propertyAccessor.GetProperty(PR_SMTP_ADDRESS) as string;
+                                    }
+                                    catch (COMException) { }
+                                }
+                            }
+                            finally
+                            {
+                                if (propertyAccessor != null)
+                                {
+                                    Marshal.ReleaseComObject(propertyAccessor);
+                                    propertyAccessor = null;
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+                finally
+                {
+                    if (sender != null)
+                    {
+                        Marshal.ReleaseComObject(sender);
+                        sender = null;
+                    }
+                }
+            }
+            else
+            {
+                return _mail.SenderEmailAddress;
+            }
+
+            return _mail.SenderEmailAddress;
+        }
+
+        internal static Outlook.Account GetAccount(this Outlook.MailItem _mail)
+        {
+            if (_mail == null) throw new ArgumentNullException();
+
+            Outlook.Folder folder = null;
+            try
+            {
+                folder = _mail.Parent as Outlook.Folder;
+                return GetAccount(folder);
+            }
+            finally
+            {
+                if (folder != null)
+                {
+                    Marshal.ReleaseComObject(folder);
+                    folder = null;
+                }
+            }
+        }
+
+        internal static Outlook.Account GetAccount(this Outlook.Folder _folder)
+        {
+            Outlook.Store store = null;
+            Outlook.NameSpace session = null;
+            Outlook.Accounts accounts = null;
+
+            try
+            {
+                store = _folder.Store;
+                session = _folder.Session;
+                accounts = session.Accounts;
+
+                foreach (Outlook.Account account in accounts)
+                {
+                    Outlook.Store accountStore = null;
+
+                    try
+                    {
+                        accountStore = account.DeliveryStore;
+                        if (accountStore.StoreID == store.StoreID)
+                        {
+                            return account;
+                        }
+                        else
+                        {
+                            Marshal.ReleaseComObject(account);
+                        }
+                    }
+                    finally
+                    {
+                        if (accountStore != null)
+                        {
+                            Marshal.ReleaseComObject(accountStore);
+                            accountStore = null;
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                if (store != null)
+                {
+                    Marshal.ReleaseComObject(store);
+                    store = null;
+                }
+
+                if (session != null)
+                {
+                    Marshal.ReleaseComObject(session);
+                    session = null;
+                }
+
+                if (accounts != null)
+                {
+                    Marshal.ReleaseComObject(accounts);
+                    accounts = null;
+                }
+            }
+
+            return null;
         }
 
         internal static string GetHeaders(this Outlook.MailItem _mailItem)
