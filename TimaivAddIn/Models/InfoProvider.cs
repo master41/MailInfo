b877ba676bdb1215c95f9db867fceebcb85e9b4a
Andrew Stoliar <poisonconsumed@gmail.com>
2018-07-17 16:10:31 +0300

    +Models

---
+++ TimaivAddIn/Models/InfoProvider.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace TimaivAddIn.Models
+{
+    class InfoProvider
+    {
+        internal Outlook.MailItem Source { get; private set; }
+
+        internal InfoProvider(Outlook.MailItem mailItem)
+        {
+            Source = mailItem;
+        }
+
+        internal MailItemInfo GetInfo()
+        {
+            MailItemInfo info = new MailItemInfo
+            {
+                // ...
+
+                HeadersInfo = ParseHeaders(Source.GetHeaders())
+            };
+
+            return info;
+        }
+
+        internal MailItemHeadersInfo ParseHeaders(string messageHeaders)
+        {
+            // ...
+
+            return new MailItemHeadersInfo();
+        }
+    }
+}
